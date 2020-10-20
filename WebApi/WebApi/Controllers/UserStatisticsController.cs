d5894aa129c060e9ce327f1fd85a626c5ad13744
BylybaIlyaStudy <73159350+BylybaIlyaStudy@users.noreply.github.com>
2020-10-20 14:33:04 +0700

    Add files via upload

---
+++ WebApi/WebApi/Controllers/UserStatisticsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserStatisticsController : ControllerBase
+    {
+        private List<UserStatistics> userStatistics = new List<UserStatistics>();
+
+        public UserStatisticsController()
+        {
+            userStatistics.Add(new UserStatistics("ivan", DateTime.Now, "1.0.0", "android"));
+            userStatistics.Add(new UserStatistics("pavel", DateTime.Now, "1.0.0", "android"));
+        }
+
+        [HttpGet]
+        public IEnumerable<UserStatistics> Get()
+        {
+            return userStatistics;
+        }
+
+        [HttpPost]
+        public ActionResult<UserStatistics> Post(UserStatistics user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            userStatistics.Add(user);
+            return Ok(user);
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/UserStatistics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class UserStatistics
+    {
+        public string NameOfNode { get; private set; }
+        public DateTime DateTimeOfLastStatistics { get; private set; }
+        public string VersionOfClient { get; private set; }
+        public string TypeOfDevice { get; private set; }
+
+        public UserStatistics(string nameOfNode, DateTime dateTime, string versionOfClient, string typeOfDevice)
+        {
+            this.NameOfNode = nameOfNode;
+            this.DateTimeOfLastStatistics = dateTime;
+            this.VersionOfClient = versionOfClient;
+            this.TypeOfDevice = typeOfDevice;
+        }
+
+        public UserStatistics(string nameOfNode, string versionOfClient, string typeOfDevice) :
+            this(nameOfNode, DateTime.Now, versionOfClient, typeOfDevice)
+        { }
+
+        public UserStatistics() : this("", DateTime.MinValue, "", "") { }
+    }
+}
