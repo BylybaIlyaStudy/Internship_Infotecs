8ab39753e871def7863a970a0fd4159195292ed4
BylybaIlyaStudy <73159350+BylybaIlyaStudy@users.noreply.github.com>
2020-10-21 19:00:40 +0700

    Add files via upload

---
+++ SPA_blazor/SPA_blazor/Data/UserStatisticsService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infotecs.SPA_blazor.Data
+{
+    public class UserStatisticsService
+    {
+        public List<UserStatistics> GetUserStatistics()
+        {
+            //List<UserStatistics> userStatistics = null;
+
+            WebRequest request = WebRequest.Create("https://localhost:5001/api/userstatistics");
+            WebResponse response = request.GetResponse();
+
+            StreamReader stream = new StreamReader(response.GetResponseStream());
+            string json = stream.ReadToEnd();
+
+            List<UserStatistics> users = JsonSerializer.Deserialize<List<UserStatistics>>(json);
+
+            return users;
+        }
+    }
+}
